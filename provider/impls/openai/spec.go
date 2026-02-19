f58f73e63fa069fa34bdc6baf9c3f4f7cad510e6
徐文彬 <1473157698@qq.com>
2026-02-19 11:29:53 +0800

    接入阿里云百炼和火山引擎

--- provider/impls/openai/spec.go
+++ provider/impls/openai/spec.go
@@ -6,4 +6,6 @@ func init() {
 	base.Register("openai", NewOpenAICompatSpec("openai", "https://api.openai.com/v1"))
 	base.Register("moonshot", NewOpenAICompatSpec("moonshot", "https://api.moonshot.cn/v1"))
 	base.Register("deepseek", NewOpenAICompatSpec("deepseek", "https://api.deepseek.com/v1"))
+	base.Register("dashscope", NewOpenAICompatSpec("dashscope", "https://dashscope.aliyuncs.com/compatible-mode/v1"))
+	base.Register("volcengine", NewOpenAICompatSpec("volcengine", "https://ark.cn-beijing.volces.com/api/v3"))
 }
